4ce52ed7f42165285f5d82c519b4330fbf1f8d1d
Enow Mbi <benowmbi@yahoo.com>
2022-10-17 08:34:58 +0100

    Clean up generator config

--- config/application.rb
+++ config/application.rb
@@ -30,9 +30,5 @@ module ECommerceWithMongodb
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.generators do |g|
-      g.orm :mongoid
-    end
   end
 end
