f1926972c2f746f7b1d8542fb47a4e0596c3145c
Enow Mbi <benowmbi@yahoo.com>
2022-10-22 10:00:25 +0100

    Sanitize devise parameters for signup and account updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,27 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
+    whitelist_devise_parameters_for_signup
+    whitelist_devise_parameters_for_account_update
+  end
+
+  private
+
+  # @address_attributes = %I[country city state street1 street2 zip]
+
+  def whitelist_devise_parameters_for_signup
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [billing_address: %I[country city state street1 street2 zip]])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [shipping_address: %I[country city state street1 street2 zip]])
+  end
 
+  def whitelist_devise_parameters_for_account_update
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [billing_address: %I[country city state street1 street2 zip]])
+    devise_parameter_sanitizer.permit(:account_update, keys: [shipping_address: %I[country city state street1 street2 zip]])
   end
 end
