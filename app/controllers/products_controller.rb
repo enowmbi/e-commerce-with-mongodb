0d26e0f0337ceeceec12de4cefbcc420b57b9e72
Enow Mbi <benowmbi@yahoo.com>
2022-10-24 04:34:55 +0100

    Whitelist tags and categories attributes --accepting arrays

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -66,6 +66,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:name, :tags, :categories, :skus)
+      params.require(:product).permit(:name, tags: [], categories: [], skus: [])
     end
 end
